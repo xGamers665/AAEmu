7f382eac919177697fef8d3b992398ccfe741892
xGamers665 <xgamers665@gmail.com>
2019-09-30 15:49:04 +0300

    openportaloutlandreagents.cs

---
+++ AAEmu.Game/ Модели/ Игры/ OpenPortal/openportaloutlandreagents.cs
@@ -0,0 +1,11 @@
+namespace AAEmu.Game.Models.Game.OpenPortal
+{
+    public class openportaloutlandreagents
+    {
+        public uint Id { get; set; }
+        public uint OpenPortalEffectId { get; set; }
+        public uint ItemId { get; set; }
+        public int Amount { get; set; }
+        public int Priority { get; set; }
+    }
+}
